a1c6d75321fa0d42aac930662bc67266899d5dd0
CouLiBaLy-B <icoulbi4@gmail.com>
2023-01-31 22:21:31 +0100

    Version finale

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,64 +1,79 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <limits.h>
-using namespace std; 
+#include <climits>
  
-// Définir le nombre de sommets dans le graphe 
-#define V 9 
-  
-// Fonction pour trouver le plus court chemin de source à destinationination 
-int dijkstra(int graph[V][V], int source, int destination) 
-{ 
-    // Tableau pour stocker la distance du sommet à la source  
-    int dist[V];  
+using namespace std;
+ 
+// Nombre de sommets dans le graphe
+#define V 7
+ 
+// Fonction pour trouver le sommet avec la distance minimale, à partir duquel on peut encore atteindre un autre sommet
+int minDistance(int dist[], bool visited[])
+{
+    // Initialisation de la distance minimale à INT_MAX qui est le plus grand entier possible.  
+    int min = INT_MAX, min_index;
 
-    // Tableau booléen pour vérifier si un sommet a déjà été visité ou non  
-    bool visited[V];  
+    for (int v = 0; v < V; v++) {
 
-    // Initialisation des distances et des visites à false  
-    for (int i = 0; i < V; i++) { 
-        dist[i] = INT_MAX; 
-        visited[i] = false; 
+        if (visited[v] == false && dist[v] <= min) {
+
+            min = dist[v], min_index = v;
+
+        }
+
     }
 
-    // Distance de la source à elle-même est toujours 0  
-    dist[source] = 0;  
-
-    // Trouver le plus court chemin pour tous les sommets  
-    for (int count = 0; count < V - 1; count++) {
-
-        // Prendre le sommet avec la distance minimale parmi ceux qui n'ont pas encore été visités.  														     u     v     w     x     y     z      a      b      c      d      e       f       g       h       i       j        k        l         m         n          o          p           q            r            s            t            u            v             w              x               y                z                 a                  b                   c                    d                     e                      f                       g                        h                         i                          j                           k                            l                             m                              n                               o                                p                                  q                                   r                                    s                                     t                                      u                                       v                                        w 
-
-        int min_dist = INT_MAX, min_index;  
-
-        for (int v = 0; v < V; v++) {
-
-            if (visited[v] == false && dist[v] <= min_dist) {
-
-                min_dist = dist[v]; 
-
-                min_index = v; 
-
-            }
-
-        }
-
-        // Marquer le sommet choisi comme visité et inclure sa distance dans la solution finale.  
-
-        visited[min_index] = true;  
-
-        for (int j = 0; j < V; j++) {
-
-            if (!visited[j] && graph[min_index][j] && dist[min_index] != INT_MAX && dist[min_index] + graph[min_index][j] < dist[j]) {
-
-                dist[j] = dist[min_index] + graph[min_index][j]; 
-
-            }
-
-        }
-
-    } 
-
-    return dist[destination]; 
+    return min_index;
 }
+
+ 
+// Affiche la solution  
+void printSolution(int dist[]) {
+
+    cout << "Distance des sommets du graphe \n";
+
+    for (int i = 0; i < V; i++) {
+
+        cout << i << " \t\t " << dist[i] << endl;
+
+    }    
+}
+
+ 
+// Algorithme de Dijkstra pour trouver la distance minimale connaissant la matrice d'adjacence.  
+void dijkstra(int graph[V][V], int src) {     // src est le sommet source.  
+
+    int dist[V];      // Tableau pour stocker les distances des sommets par rapport au sommet source.  
+
+                       // visited[] est un tableau booléen qui indique si un sommet a déjà été visité ou non.    
+
+    bool visited[V];   // Initialisation de toutes les distances à l'infini et tous les sommets non visités.  
+
+    for (int i = 0; i < V; i++) {         // On initialise toutes les distances à l'infini et tous les sommets non visités.  
+
+        dist[i] = INT_MAX, visited[i] = false;     }      // La distance du sommet source à elle-même est toujours nulle.    
+
+    dist[src] = 0;      // Trouver le chemin le plus court pour chaque sommet      
+    for (int count = 0; count < V - 1; count++) {          
+        // Trouver le sommet avec la distance minimale, à partir duquel on peut encore atteindre un autre sommet          
+        int u = minDistance(dist, visited);          
+         // Marquer le noeud comme visité          
+        visited[u] = true;          
+        // Mettre à jour la distance des voisins du noeud choisi          
+        for (int v = 0; v < V ; v++)              
+            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX && dist[u]+graph [u][v] < dist [v])                  
+                dist [v]=dist [u]+graph [u][v];      }      
+            printSolution(dist); }      
+
+int main() {
+
+    int graph [V][V]={{4  , 0  ,  0  ,  0  ,  0  ,    5  , 0},
+                      {3  , 1  ,  0  ,  10 ,  3  ,   11  , 0},
+                      {0  , 1  ,  0  ,  7  ,  0  ,   0   , 0},
+                      {0  , 0  ,  0  ,  7  ,   0 ,    0  , 0},
+                      {0  , 0  ,  0  ,  4  ,  4  ,    0  , 0},
+                      {1  , 0  ,  0  ,  0  ,  0  ,    9  , 0},
+                      {5  , 0  ,  0  ,  0  ,  4  ,    0  , INT_MAX}
+                 };        
+    dijkstra(graph, 2);      
+    return 0 ;     }
