c4f3057a3adfaf1cce8a3c62ccc9ff1750428dd8
AhmedOsman00py <78887980+AhmedOsman00py@users.noreply.github.com>
2023-01-31 22:57:39 +0100

    update input examples
    adding input with adjency matrix

--- input_example.R
+++ input_example.R
@@ -8,7 +8,7 @@ vertices <- 1:7
 from_ <- c(3, 3, 2, 2, 2, 2, 6, 1, 7, 7, 5)
 to_   <- c(4, 2, 4, 5, 6, 1, 1, 6, 1, 5, 4)  
 
-# --- Input 
+# --- Input avec Data Frame
 edges <- data.frame(from   = from_,
                     to     = to_,
                     weight = c(7, 1, 1, 3, 1, 3, 1, 5, 5, 4, 4)) 
@@ -31,6 +31,37 @@ plot(g,
      edge.label=E(g)$weight, 
      edge.label.cex=1, 
      vertex.size = 25, 
+     edge.arrow.size = 0.5, 
+     layout = coords)
+
+
+# --- Input avec Matrice d'adjacence
+
+                    #  1  2  3  4  5  6  7
+adj_matrix <- matrix(c(0, 0, 0, 0, 0, 5, 0, 
+                       3, 0, 0, 1, 3, 1, 0, 
+                       0, 1, 0, 7, 0, 0, 0, 
+                       0, 0, 0, 0, 0, 0, 0, 
+                       0, 0, 0, 4, 0, 0, 0,
+                       1, 0, 0, 0, 0, 0, 0, 
+                       5, 0, 0, 0, 4, 0, 0), ncol = 7, byrow = TRUE)
+
+g <- graph.adjacency(adj_matrix, mode = "directed", weighted = TRUE)
+
+
+coords <- matrix(c(8, 0,  # 4
+                   4, 0,  # 2
+                   2, 2,  # 1
+                   0, 0,  # 7
+                   2, -2, # 6
+                   6, 2,  # 3
+                   6, -2),# 5
+                 byrow = TRUE, ncol = 2)
+
+plot(g, 
+     edge.label=E(g)$weight, 
+     edge.label.cex=1, 
+     vertex.size = 25,
      edge.arrow.size = 0.5, 
      layout = coords)
 
